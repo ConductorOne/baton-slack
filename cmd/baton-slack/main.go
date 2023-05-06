a59896bf5108693c9ef519bb0e12798f4f671746
Justin Gallardo <justin.gallardo@conductorone.com>
2023-05-05 18:02:01 -0700

    ConductorOne/baton-slack => conductorone/baton-slack

--- cmd/baton-slack/main.go
+++ cmd/baton-slack/main.go
@@ -5,11 +5,11 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/ConductorOne/baton-slack/pkg/connector"
 	"github.com/conductorone/baton-sdk/pkg/cli"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
 	"github.com/conductorone/baton-sdk/pkg/sdk"
 	"github.com/conductorone/baton-sdk/pkg/types"
+	"github.com/conductorone/baton-slack/pkg/connector"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
 	"go.uber.org/zap"
 )
