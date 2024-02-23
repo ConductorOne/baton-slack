f7584cf00e508f0c35037b5d55f32c374495eb77
Logan Saso <logan.saso@conductorone.com>
2024-02-23 10:54:29 -0800

    Format

--- pkg/connector/helpers.go
+++ pkg/connector/helpers.go
@@ -2,6 +2,7 @@ package connector
 
 import (
 	"errors"
+
 	v2 "github.com/conductorone/baton-sdk/pb/c1/connector/v2"
 	"github.com/conductorone/baton-sdk/pkg/annotations"
 	"github.com/conductorone/baton-sdk/pkg/pagination"
