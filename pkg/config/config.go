cbeef0f93d1155c3ed4208f63fdd04536f9114df
Alejandro Bernal <jbernalxyz@gmail.com>
2025-09-12 09:50:25 -0500

    move config to pkg

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,4 +1,4 @@
-package main
+package config
 
 import (
 	"github.com/conductorone/baton-sdk/pkg/field"
@@ -7,36 +7,47 @@ import (
 var (
 	AccessTokenField = field.StringField(
 		"token",
+		field.WithDisplayName("Access Token"),
 		field.WithDescription("The Slack bot user oauth token used to connect to the Slack API"),
 		field.WithRequired(true),
 	)
 	EnterpriseTokenField = field.StringField(
 		"enterprise-token",
+		field.WithDisplayName("Enterprise Token"),
 		field.WithDescription("The Slack user oauth token used to connect to the Slack Enterprise Grid Admin API"),
 	)
 	SSOEnabledField = field.BoolField(
 		"sso-enabled",
+		field.WithDisplayName("SSO Enabled"),
 		field.WithDescription("Flag indicating that the SSO has been configured for Enterprise Grid Organization. Enables usage of SCIM API"),
 		field.WithDefaultValue(false),
 	)
 	GovEnvironmentField = field.BoolField(
 		"gov-env",
+		field.WithDisplayName("Gov Environment"),
 		field.WithDescription("Flag indicating to use Slack-Gov environment."),
 		field.WithDefaultValue(false),
 	)
 
+	// ConfigurationFields defines the external configuration required for the
+	// connector to run. Note: these fields can be marked as optional or
+	// required.
+	ConfigurationFields = []field.SchemaField{
+		AccessTokenField,
+		EnterpriseTokenField,
+		SSOEnabledField,
+		GovEnvironmentField,
+	}
+
+	// FieldRelationships defines relationships between the fields listed in
+	// ConfigurationFields that can be automatically validated.
 	// Every Gov Slack instance is an Enterprise Grid instance.
-	Constraints = []field.SchemaFieldRelationship{
+	FieldRelationships = []field.SchemaFieldRelationship{
 		field.FieldsDependentOn(
 			[]field.SchemaField{GovEnvironmentField},
 			[]field.SchemaField{EnterpriseTokenField},
 		),
 	}
 
-	Configuration = field.NewConfiguration([]field.SchemaField{
-		AccessTokenField,
-		EnterpriseTokenField,
-		SSOEnabledField,
-		GovEnvironmentField,
-	}, Constraints...)
+	Configuration = field.NewConfiguration(ConfigurationFields, field.WithConstraints(FieldRelationships...))
 )
