ff0f1c3ba9745e2895e9a2ea523f6b0d5d0971fd
Lauren Leach <lauren.leach@conductorone.com>
2026-02-26 17:19:18 -0800

    annotate permissions/v2 default builder

--- pkg/connector/resource_types.go
+++ pkg/connector/resource_types.go
@@ -5,6 +5,14 @@ import (
 	"github.com/conductorone/baton-sdk/pkg/annotations"
 )
 
+func capabilityPermissions(perms ...string) *v2.CapabilityPermissions {
+	cp := &v2.CapabilityPermissions{}
+	for _, p := range perms {
+		cp.Permissions = append(cp.Permissions, &v2.CapabilityPermission{Permission: p})
+	}
+	return cp
+}
+
 var (
 	resourceTypeUser = &v2.ResourceType{
 		Id:          "user",
@@ -12,7 +20,18 @@ var (
 		Traits: []v2.ResourceType_Trait{
 			v2.ResourceType_TRAIT_USER,
 		},
-		Annotations: annotations.New(&v2.SkipEntitlementsAndGrants{}),
+		Annotations: annotations.New(
+			&v2.SkipEntitlementsAndGrants{},
+			capabilityPermissions(
+				// Bot Token Scopes
+				"users:read",
+				"users:read.email",
+				"users.profile:read",
+				// User Token Scopes (Business+ SCIM)
+				"admin",
+				"admin.users:read",
+			),
+		),
 	}
 	resourceTypeWorkspace = &v2.ResourceType{
 		Id:          "workspace",
@@ -20,6 +39,19 @@ var (
 		Traits: []v2.ResourceType_Trait{
 			v2.ResourceType_TRAIT_GROUP,
 		},
+		Annotations: annotations.New(
+			capabilityPermissions(
+				// Bot Token Scopes
+				"team:read",
+				"users:read",
+				"users:read.email",
+				"channels:join",
+				"channels:read",
+				"groups:read",
+				// User Token Scopes (Business+)
+				"admin.teams:read",
+			),
+		),
 	}
 	resourceTypeUserGroup = &v2.ResourceType{
 		Id:          "userGroup",
@@ -27,6 +59,15 @@ var (
 		Traits: []v2.ResourceType_Trait{
 			v2.ResourceType_TRAIT_GROUP,
 		},
+		Annotations: annotations.New(
+			capabilityPermissions(
+				// Bot Token Scopes
+				"usergroups:read",
+				"users:read",
+				// User Token Scopes (Business+)
+				"admin.usergroups:read",
+			),
+		),
 	}
 	resourceTypeGroup = &v2.ResourceType{
 		Id:          "group",
@@ -34,12 +75,26 @@ var (
 		Traits: []v2.ResourceType_Trait{
 			v2.ResourceType_TRAIT_GROUP,
 		},
+		Annotations: annotations.New(
+			capabilityPermissions(
+				// User Token Scopes (Business+ SCIM)
+				"admin",
+				"admin.users:write",
+			),
+		),
 	}
 
 	resourceTypeWorkspaceRole = &v2.ResourceType{
 		Id:          "workspaceRole",
 		DisplayName: "Workspace Role",
-		Annotations: annotations.New(&v2.SkipGrants{}),
+		Annotations: annotations.New(
+			&v2.SkipGrants{},
+			capabilityPermissions(
+				// User Token Scopes (Business+)
+				"admin",
+				"admin.roles:read",
+			),
+		),
 		Traits: []v2.ResourceType_Trait{
 			v2.ResourceType_TRAIT_ROLE,
 		},
